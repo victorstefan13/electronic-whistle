b72c0806e90f1fb243338b6e68dc08292aedac49
Victor Efrim <vefrim@gmail.com>
2018-08-19 11:31:22 +0100

    play tone header file

---
+++ play_tone.h
@@ -0,0 +1,12 @@
+#ifndef PLAY_TONE_H
+#define PLAY_TONE_H
+
+void init_buzzer(void); //prototyping the init_buzzer function which will be used in play_tone.c file
+
+void play_buzzer(void); //prototyping the play_buzzer function which will be used in play_tone.c file
+
+int freq_change(int input); //prototyping the freq_change function which will be used in play_tone.c file
+
+#endif 
+
+
